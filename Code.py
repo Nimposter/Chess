c5128f76501fbf76ac2255a529ae7b7bf8b851d2
Nimposter <naelhumball@gmail.com>
2026-08-02 18:26:49 +0500

    Queen, Bishop, Knight Move Functionality
    Added functions to generate legal moves for queens bishops and knights (dosent check if a square is occupied for knight)
    
    moves piece of code into another function called "GetMoves()"
    this function takes input, converts coordinates to index, and calls the appropriate function for legal move generation, then checks whether entered coordinates are a legal move of that piece (loops until legal move in entered)
    
    Future additions: 
    add functions for moving King, Pawn
    rewrite Knight function logic with Loops, add checks in knight function for occupied square
    add error handling if user inputs invalid coordinates (i.e. outside the board) or tries to move an empty square

--- Code.py
+++ Code.py
@@ -7,7 +7,7 @@ Board = [["r","n","b","q","k","b","n","r"],
         ["*" for _ in range(8)],
         ["*" for _ in range(8)],
         ["*" for _ in range(8)],
-        ["P" for _ in range(8)],
+        ["*" for _ in range(8)],
         ["R","N","B","Q","K","B","N","R"]]
 
 #dictionary to convert chess coordinates to array indexes
@@ -75,22 +75,190 @@ def RookLegalMoves(f , r):
             break
         else:
             Moves.append(str(tempf) + str(tempr))
-    
     return Moves
 
+def BishopLegalMoves(f , r):
+    Moves = []
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves toward bottom-right of bishop
+        tempf += 1
+        tempr += 1
+        if tempf > 7 or tempr > 7 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards top-left of bishop
+        tempf -= 1
+        tempr -= 1
+        if tempf < 0 or tempr < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards bottom-left of bishop
+        tempf -= 1
+        tempr += 1
+        if tempr > 7 or tempf < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards top-left of bishop
+        tempf += 1
+        tempr -= 1
+        if tempf > 7 or tempr < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    return Moves
+
+def QueenLegalMoves(f , r):
+    Moves = []
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves to right of rook
+        tempf += 1
+        if tempf > 7 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves to left of rook
+        tempf -= 1
+        if tempf < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves below rook
+        tempr += 1
+        if tempr > 7 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves above rook
+        tempr -= 1
+        if tempr < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves toward bottom-right of bishop
+        tempf += 1
+        tempr += 1
+        if tempf > 7 or tempr > 7 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards top-left of bishop
+        tempf -= 1
+        tempr -= 1
+        if tempf < 0 or tempr < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards bottom-left of bishop
+        tempf -= 1
+        tempr += 1
+        if tempr > 7 or tempf < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    tempf = f
+    tempr = r
+    while True:
+        #calculates all legal moves towards top-left of bishop
+        tempf += 1
+        tempr -= 1
+        if tempf > 7 or tempr < 0 or Board [tempr][tempf] != "*":
+            break
+        else:
+            Moves.append(str(tempf) + str(tempr))
+    return Moves
+
+def KnightLegalMoves(f , r):
+    print("called")
+    Moves = []
+    tempf = f
+    tempr = r
+    if tempf + 2 <= 7:
+        if tempr + 1 <= 7:
+            Moves.append(str(tempf + 2) + str(tempr + 1))
+        if tempr - 1 >= 0:
+            Moves.append(str(tempf + 2) + str(tempr - 1))
+    if tempf - 2 >= 0:
+        if tempr + 1 <= 7:
+            Moves.append(str(tempf - 2) + str(tempr + 1))
+        if tempr - 1 >= 0:
+            Moves.append(str(tempf - 2) + str(tempr - 1))
+    if tempr + 2 <= 7:
+        if tempf + 1 <= 7:
+            Moves.append(str(tempf + 1) + str(tempr + 2))
+        if tempf - 1 >= 0:
+            Moves.append(str(tempf - 1) + str(tempr + 2))
+    if tempr - 2 >= 0:
+        if tempf + 1 <= 7:
+            Moves.append(str(tempf + 1) + str(tempr - 2))
+        if tempf - 1 >= 0:
+            Moves.append(str(tempf - 1) + str(tempr - 2))
+    return Moves
+
+def GetMoves():
+    while True:
+        Coords = input("enter Coordinates of piece to be moved: ")
+        source_file = Coordinates.get(Coords[0])
+        source_rank = Coordinates.get(Coords[1])
+        #print(source_file, source_rank)
+        print( "you have chosen",Board[source_rank][source_file])
+        match Board[source_rank][source_file].lower():
+            case "r":
+                LegalMoves = RookLegalMoves(source_file , source_rank)
+            case "b":
+                LegalMoves = BishopLegalMoves(source_file , source_rank)
+            case "q":
+                LegalMoves = QueenLegalMoves(source_file , source_rank)
+            case "n":
+                LegalMoves = KnightLegalMoves(source_file , source_rank)
+        print(LegalMoves)
+        
+        Coords2 = input("enter Coordinates of square to move to: ")
+        target_file = Coordinates.get(Coords2[0])
+        target_rank = Coordinates.get(Coords2[1])
+        for i in LegalMoves:
+            if i == str(target_file) + str(target_rank):
+                #print("Legal Move")
+                return source_rank,source_file,target_rank,target_file
+        print("illegal move")
+
 DisplayBoard()
+source_rank,source_file,target_rank,target_file = GetMoves()
 
-Coords = input("enter Coordinates of piece to be moved: ")
-file = Coordinates.get(Coords[0])
-rank = Coordinates.get(Coords[1])
-print(Board[rank][file])
-LegalMoves = RookLegalMoves(file , rank)
-print(LegalMoves)
-
-Coords2 = input("enter Coordinates of square to move to: ")
-file2 = Coordinates.get(Coords2[0])
-rank2 = Coordinates.get(Coords2[1])
-
+"""
 match Board[rank][file].lower():
     case "r":
         print("rook")
@@ -102,8 +270,10 @@ match Board[rank][file].lower():
         print("queen")
     case "k":
         print("king")
-Board[rank2][file2] = Board[rank][file]
-Board[rank][file] = "*"
+
+"""
+Board[target_rank][target_file] = Board[source_rank][source_file]
+Board[source_rank][source_file] = "*"
 DisplayBoard()
 
 #row = int(input("what row would u like to move"+"\n"))
