abddf83420dc6888a91c211da233752f5e3c135d
Nimposter <naelhumball@gmail.com>
2026-08-09 16:31:51 +0500

    Capture Functionality
    added a helper function IsEnemy() to check whether a specific square contains your piece or your opponents (checks whose turn it is, and whether a piece is uppercase and lowercase)
    
    implemented capture functionality in all pieces (except king) using the helper function. pieces can now only capture enemy squares
    
    added piece selection validation, ie you can only move your pieces, and not your opponent's. program will keep asking until you select a valid piece
    
    future additions:
    helper function to determine if a specific piece is attacked
    use that funtion to determine king legal moves, and checkmate + stalemate

--- Code.py
+++ Code.py
@@ -53,10 +53,13 @@ def SlidingLegalMoves(f , r , piece):
         tempf = f
         tempr = r
         while True:
-            #calculates all legal moves to right of rook
             tempf += x
             tempr += y
-            if 7 < tempf or tempf < 0 or 7 < tempr or tempr < 0 or Board [tempr][tempf] != "*":
+            if not OnBoard(tempf, tempr):
+                break
+            elif Board [tempr][tempf] != "*":
+                if IsEnemy(tempf , tempr):
+                    Moves.append(str(tempf) + str(tempr))
                 break
             else:
                 Moves.append(str(tempf) + str(tempr))
@@ -68,8 +71,11 @@ def KnightLegalMoves(f , r):
                       (-1, +2) , (-1, -2)]
     Moves = []
     for x , y in relative_offset:
-        if 0 <= f + x <= 7 and 0 <= r + y <= 7:
-            Moves.append(str(f + x) + str(r + y))
+        if OnBoard(f + x, r + y):
+            if Board[r + y][f + x] == "*":
+                Moves.append(str(f + x) + str(r + y))
+            elif IsEnemy(f + x, r + y):
+                Moves.append(str(f + x) + str(r + y))
     return Moves
 
 def PawnLegalMoves(f , r):
@@ -79,11 +85,10 @@ def PawnLegalMoves(f , r):
             Moves.append(str(f) + str(r - 1))
             if r == 6 and Board[r - 2][f] == "*":
                 Moves.append(str(f) + str(r - 2))
-            print(Board[r - 1][f - 1])
-        if OnBoard(f - 1 , r - 1) and Board[r - 1][f - 1] != "*":
+        if OnBoard(f - 1 , r - 1) and Board[r - 1][f - 1] != "*" and IsEnemy(f - 1 , r - 1):
             print("left capture")
             Moves.append(str(f - 1) + str(r - 1))
-        if OnBoard(f + 1 , r - 1) and Board[r - 1][f + 1] != "*":
+        if OnBoard(f + 1 , r - 1) and Board[r - 1][f + 1] != "*" and IsEnemy(f + 1 , r - 1):
             print("right capture")
             Moves.append(str(f + 1) + str(r - 1))
     else:
@@ -91,42 +96,68 @@ def PawnLegalMoves(f , r):
             Moves.append(str(f) + str(r + 1))
             if r == 1 and Board[r + 2][f] == "*":
                 Moves.append(str(f) + str(r + 2))
-        if OnBoard(f - 1 , r + 1) and Board[r + 1][f - 1] != "*":
+        if OnBoard(f - 1 , r + 1) and Board[r + 1][f - 1] != "*" and IsEnemy(f - 1 , r + 1):
             print("left capture")
             Moves.append(str(f - 1) + str(r + 1))
-        if OnBoard(f + 1 , r + 1) and Board[r + 1][f + 1] != "*":
+        if OnBoard(f + 1 , r + 1) and Board[r + 1][f + 1] != "*" and IsEnemy(f + 1 , r + 1):
             print("right capture")
             Moves.append(str(f + 1) + str(r + 1))
         
     return Moves
 
 def GetMoves():
+    #repeats until user has entered a valid move
     while True:
-        Coords = input("enter Coordinates of piece to be moved: ")
-        source_file = Coordinates.get(Coords[0])
-        source_rank = Coordinates.get(Coords[1])
-        #print(source_file, source_rank)
+        
+        #loops until user has selected their piece
+        while True:
+            #gets input for the square of the piece to move
+            Coords = input("enter Coordinates of piece to be moved: ")
+            source_file = Coordinates.get(Coords[0])
+            source_rank = Coordinates.get(Coords[1])
+            
+            #checks user has selected their piece, not opponents or empty square
+            if WhiteMove and Board[source_rank][source_file] == Board[source_rank][source_file].upper() != "*":
+                break
+            if not WhiteMove and Board[source_rank][source_file] == Board[source_rank][source_file].lower() != "*":
+                break
+            else:
+                print("please select your own piece")
+        
         print( "you have chosen",Board[source_rank][source_file])
+        
+        #determines what piece has been selected and calls the respective move generation function
         match Board[source_rank][source_file].lower():
-            case "q" | "b" | "r":
-                LegalMoves = SlidingLegalMoves(source_file , source_rank , Board[source_rank][source_file].lower())
-            case "n":
-                LegalMoves = KnightLegalMoves(source_file , source_rank)
-            case "p":
-                LegalMoves = PawnLegalMoves(source_file , source_rank)
+                case "q" | "b" | "r":
+                    LegalMoves = SlidingLegalMoves(source_file , source_rank , Board[source_rank][source_file].lower())
+                case "n":
+                    LegalMoves = KnightLegalMoves(source_file , source_rank)
+                case "p":
+                    LegalMoves = PawnLegalMoves(source_file , source_rank)
+        
         print(LegalMoves)
         
+        #gets input from user of square to move to
         Coords2 = input("enter Coordinates of square to move to: ")
         target_file = Coordinates.get(Coords2[0])
         target_rank = Coordinates.get(Coords2[1])
+        
+        #checks whether move user has entered is a legal move or not
         for i in LegalMoves:
             if i == str(target_file) + str(target_rank):
                 #print("Legal Move")
+                #if entered move is legal, it returns to the main program
                 return source_rank,source_file,target_rank,target_file
         print("illegal move")
 
 def OnBoard(f , r):
     return 0 <= f <= 7 and 0 <= r <= 7
+
+def IsEnemy(f , r):
+    if Board[r][f] == Board[r][f].upper():
+        return not WhiteMove
+    else:
+        return WhiteMove
 
 while not Checkmate and not Stalemate:
     DisplayBoard()
